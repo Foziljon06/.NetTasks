8f81d36bab4ff27e5c6710b686dac5c33038f22f
Foziljon06 <fozilxolmatov9@gmail.com>
2024-12-16 20:18:06 +0300

    Birinchi bolim 3 - zadanyasi

--- Program.cs
+++ Program.cs
@@ -82,8 +82,166 @@
 //                                       Task 3
 
 
-        Console.Write("Raqamni kiriting: ");
-        int number = int.Parse(Console.ReadLine());
+        // Console.Write("Raqamni kiriting: ");
+        // int number = int.Parse(Console.ReadLine());
 
-        string result = (number % 2 == 0) ? "Juft" : "Toq";
-        Console.WriteLine(result);
+        // string result = (number % 2 == 0) ? "Juft" : "Toq";
+        // Console.WriteLine(result);
+
+
+
+
+         //  //                Basic.Ketma-ketlik bo'limi uchun 3-amaliy vazifa
+
+
+//                                       Task 1
+
+
+
+    //     Console.Write("Sonni kiriting: ");
+    //     if (int.TryParse(Console.ReadLine(), out int number))
+    //     {
+    //         if (number > 1 && IsPrime(number))
+    //         {
+    //             Console.WriteLine("Tub");
+    //         }
+    //         else
+    //         {
+    //             Console.WriteLine("Tub emas");
+    //         }
+    //     }
+    //     else
+    //     {
+    //         Console.WriteLine("Xato: Iltimos, butun son kiriting!");
+    //     }
+    
+
+    // static bool IsPrime(int num)
+    // {
+    //     for (int i = 2; i <= Math.Sqrt(num); i++)
+    //     {
+    //         if (num % i == 0)
+    //             return false;
+    //     }
+    //     return true;
+    // }
+
+
+
+//                                       Task 2
+
+
+        // Console.Write("Sonni kiriting: ");
+        // if (int.TryParse(Console.ReadLine(), out int number))
+        // {
+        //     Console.WriteLine("Bo'luvchilar:");
+        //     for (int i = 2; i <= 10; i++)
+        //     {
+        //         if (number % i == 0)
+        //             Console.WriteLine(i);
+        //     }
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Xato: Iltimos, butun son kiriting!");
+        // }
+    
+
+
+
+//                                       Task 3
+
+        // Console.Write("Sonni kiriting: ");
+        // if (int.TryParse(Console.ReadLine(), out int baseNumber))
+        // {
+        //     Console.Write("Darajani kiriting (musbat butun son): ");
+        //     if (int.TryParse(Console.ReadLine(), out int power) && power >= 0)
+        //     {
+        //         double result = Math.Pow(baseNumber, power);
+        //         Console.WriteLine($"Natija: {result}");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("Xato: Daraja musbat butun son bo'lishi kerak!");
+        //     }
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Xato: Iltimos, butun son kiriting!");
+        // }
+
+
+//                                       Task 4
+
+
+// Console.WriteLine("Harajat miqdorlarini vergul bilan ajratib kiriting (masalan: 150, 230, 80, 120): ");
+//         string input = Console.ReadLine();
+
+//         try
+//         {
+//             // Vergul bilan ajratilgan ma'lumotlarni sonlarga aylantirish
+//             int[] amounts = input.Split(',')
+//                                  .Select(x => int.Parse(x.Trim()))
+//                                  .ToArray();
+
+//             // Yig'indini hisoblash
+//             int total = amounts.Sum();
+
+//             Console.WriteLine($"Yig'indi: {total}");
+//         }
+//         catch
+//         {
+//             Console.WriteLine("Xato: Faqat raqamlarni kiriting va ularni vergul bilan ajrating!");
+//         }
+
+
+// //                                       Task 5
+
+
+// Console.Write("Sonni kiriting: ");
+//         if (int.TryParse(Console.ReadLine(), out int number) && number >= 0)
+//         {
+//             if (IsArmstrong(number))
+//             {
+//                 Console.WriteLine($"{number} Armstrong son");
+//             }
+//             else
+//             {
+//                 Console.WriteLine($"{number} Armstrong son emas");
+//             }
+//         }
+//         else
+//         {
+//             Console.WriteLine("Xato: Iltimos, musbat butun son kiriting!");
+//         }
+    
+
+//     static bool IsArmstrong(int num)
+//     {
+//         string digits = num.ToString(); // Sonni qatorga aylantirish
+//         int power = digits.Length; // Son raqamlarining o'lchami
+//         int sum = 0;
+
+//         foreach (char digit in digits)
+//         {
+//             int d = int.Parse(digit.ToString()); // Har bir raqamni ajratib olish
+//             sum += (int)Math.Pow(d, power); // Raqamni o'lcham darajasiga ko'tarib qo'shish
+//         }
+
+//         return sum == num;
+//     }
+
+
+
+//                                       Task 6
+
+        Console.Write("Sonni kiriting: ");
+        if (int.TryParse(Console.ReadLine(), out int number) && number > 0)
+        {
+            int count = CountProperDivisors(number);
+            Console.WriteLine($"Sifatli bo'luvchilar soni: {count}");
+        }
+        else
+        {
+            Console.WriteLine("Xato: Iltimos, musbat butun son kiriting!");
+        }
